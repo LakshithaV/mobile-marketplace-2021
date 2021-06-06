3abd89983a35757524a23b03457910c4932d6bd5
LakshithaV <lakshithaviraj@hotmail.com>
2021-06-06 23:29:48 +0530

    Profile Image Upload Completed!

--- src/app/profile-edit/profile-edit.page.ts
+++ src/app/profile-edit/profile-edit.page.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { Router } from '@angular/router';
 import { LoadingController, ToastController } from '@ionic/angular';
-import { merge } from 'rxjs';
+import { merge, Observable } from 'rxjs';
 import { AuthService } from '../services/authentication.service';
+
+export interface imageData{
+  fileName: string;
+  filePath: string;
+  size: string;
+}
 
 @Component({
   selector: 'app-profile-edit',
@@ -14,14 +21,29 @@ export class ProfileEditPage implements OnInit {
   userId: string;
   name: string;
   phone: string;
+  password: string;
+  fileName: string;
+  fileSize: string;
+  isLoading: boolean;
+  isLoaded: boolean;
+  percentage: Observable<any>;
+  private imageCollection: AngularFirestoreCollection<imageData>;
+  imagefile: Observable<imageData[]>;
+  imageUpload: AngularFireUploadTask;
 
   constructor(
     private auth: AuthService,
     private afs: AngularFirestore,
     private loadingCtrl: LoadingController,
     private toaster: ToastController,
-    private router: Router
-  ) { }
+    private router: Router,
+    private loading: LoadingController,
+    private strg: AngularFireStorage,
+  ) 
+  { 
+    this.isLoading = false;
+    this.isLoaded = false;
+  }
 
   ngOnInit() {
     this.auth.user$.subscribe(user => {
@@ -65,6 +87,46 @@ export class ProfileEditPage implements OnInit {
     });
 
     toast.present();
+  }
+
+
+  async uploadImagetoFirebase(event){
+
+    const load = await this.loading.create({
+      spinner: 'dots',
+    })
+    load.present();
+
+    const file = event.target.files;
+    console.log(file);
+    var fileName = file[0];
+    console.log(fileName);
+
+    if(fileName.type.split('/')[0] !=="image"){
+      console.error("File is not an Image");
+      return;
+    }
+
+    this.isLoaded = false;
+    this.isLoading = true;
+
+    const path = `loginUploads/${new Date().getTime()}_${fileName.name}`;
+
+    var fileRef = this.strg.ref(path);
+
+    this.imageUpload = this.strg.upload(path, fileName);
+    this.loading.dismiss();
+    this.percentage = this.imageUpload.percentageChanges();
+
+    this.imageUpload.then( res=> {
+      var imagefile = res.task.snapshot.ref.getDownloadURL();
+      imagefile.then( downloadableUrl=> {
+        console.log("URL", downloadableUrl);
+        this.afs.collection('user').doc(this.userId).set({
+          photoUrl: downloadableUrl
+        },{merge: true});
+      })
+    })
   }
 
 }
